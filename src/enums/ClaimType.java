3ee8f9520454c6a0c257da0ef1271b87e9ea1127
Mohamed Hamouch <med10hamouch@gmail.com>
2025-09-24 17:28:39 +0100

    add constructor and label to enums

--- src/enums/ClaimType.java
+++ src/enums/ClaimType.java
@@ -1,7 +1,22 @@
 package enums;
 
 public enum ClaimType {
-    CAR_ACCIDENT,
-    HOME_DAMAGE,
-    ILLNESS
+    CAR("Car Accident"),
+    HOME("Home Damage"),
+    HEALTH("Illness");
+
+    private final String displayName;
+
+    ClaimType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
--- src/enums/ContractType.java
+++ src/enums/ContractType.java
@@ -1,7 +1,22 @@
 package enums;
 
 public enum ContractType {
-    CAR,
-    HOME,
-    HEALTH
+    CAR("Car"),
+    HOME("Home"),
+    HEALTH("Health");
+
+    private final String displayName;
+
+    ContractType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
