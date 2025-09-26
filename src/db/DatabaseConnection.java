a35e962989128c9d0c4c9e4908a6051c691d5ce0
Mohamed Hamouch <med10hamouch@gmail.com>
2025-09-26 11:49:43 +0100

    DB connection  classusing singletonÃ

---
+++ src/db/DatabaseConnection.java
@@ -0,0 +1,20 @@
+package db;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/insuranceDb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "0000";
+
+    private static Connection connection;
+
+    private DatabaseConnection() {}
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+}
